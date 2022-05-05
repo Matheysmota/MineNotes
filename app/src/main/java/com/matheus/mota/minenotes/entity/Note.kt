aa8bd9924ff95a906b2fb213a683307c194706ed
Matheus <Matheusmotacosta1@gmail.com>
2022-05-05 17:10:16 -0300

    Entity/Model Created

--- app/src/main/java/com/matheus/mota/minenotes/entity/Note.kt
+++ app/src/main/java/com/matheus/mota/minenotes/entity/Note.kt
@@ -1,8 +1,5 @@
 package com.matheus.mota.minenotes.data.entity
 
-import android.content.res.ColorStateList
-import android.graphics.Color
-
 data class Note(
     val noteTittle: String,
     val noteDescription: String,
